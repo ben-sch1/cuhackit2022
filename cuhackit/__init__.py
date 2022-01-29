0d8071b99e97250c47cde674bb5b22f6aca4dea9
ben-sch1 <ben-sch1@users.noreply.github.com>
2022-01-29 16:15:08 -0500

    beginnings of post implentation

--- cuhackit/__init__.py
+++ cuhackit/__init__.py
@@ -1,8 +1,28 @@
 ### ~/.config/meerschaum/plugins/example.py
 
 from meerschaum.plugins import api_plugin
+from typing import Optional
 
 app = None
+
+def enforce_login(username):
+    """
+    Make sure the username cookie is set.
+    """
+    if username is None:
+        raise Exception("Not authenticated!")
+
+#the database we are working with
+INSTANCE_LABEL = "sql:local"
+
+import meerschaum as mrsm
+
+# a pipe is a table with a data/time access
+posts_pipe = mrsm.Pipe(
+    "data", "posts", instance = INSTANCE_LABEL,
+    #columns to create in database that will be used in the website
+    columns = {"datetime": "time", "id": "postID"}
+)
 
 @api_plugin
 def init_plugin(_app):
@@ -11,10 +31,15 @@ def init_plugin(_app):
     """
     global app
     app = _app
-    from .posts import get_posts
 
-    @app.get('/my/new/path')
-    def new_path():
-        return {'message': 'Hello, World!'}
+    # from meerschaum.api._oauth2
+    # from meerschaum.api.routes._login import load_user, login
+    # from .posts import get_posts
 
+    # from meerschaum.api import manager
+    import fastapi
 
+    @app.get('/bar')
+    def bar(username: Optional[str] = Cookie(None)):
+        enforce_login(username)
+        return {'bar': 'foo'}
--- cuhackit/posts.py
+++ cuhackit/posts.py
@@ -1,5 +1,42 @@
-from . import app
+from . import app, posts_pipe
+from meerschaum.api import manager
+import meerschaum as mrsm
+import uuid
+import datetime
+import fastapi
+
+#allow us to connect to the sql database on the sever
+conn = mrsm.get_connector("sql", "local")
 
 @app.get('/posts')
 def get_posts():
     return {'test': 'foo'}
+
+# @app.post('/posts')
+# def create_post(
+#         curr_user = fastapi.Depends(manager),
+#     ):
+#     print(curr_user.username)
+#     return {"username": curr_user.username}
+
+    #adding new data to the table (insert to table in SQL)
+    # posts_pipe.sync(
+    #     {
+    #         #uuid: rand hex generation
+    #         "postID": [str(uuid.uuid4())],
+    #         #get current time
+    #         "time": [datetime.datetime.utcnow()],
+    #         #"user": user
+    #
+    #     }
+    # )
+    # return {'test': 'foo'}
+
+# @app.post('/user')
+# def create_user():
+#     return
+#
+#
+# @app.post('/post/comment')
+# def create_comment():
+#     return
